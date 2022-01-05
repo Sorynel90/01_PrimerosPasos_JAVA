7b2fcfba1a4ca799ea7d7ecd4035c894d142b968
Sorynel90 <sorinpetrut.niculae@gmail.com>
2022-01-05 23:01:21 +0100

    poo v3

---
+++ src/poo_II/Pruebas_Final_Static.java
@@ -0,0 +1,61 @@
+package poo_II;
+
+public class Pruebas_Final_Static {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Empleados trabajador1 = new Empleados("Paco");
+		Empleados trabajador2 = new Empleados("Ana");
+		Empleados trabajador3 = new Empleados("Antonio");
+		Empleados trabajador4 = new Empleados("Maria");
+		
+		trabajador1.cambiaSeccion("RRHH");
+		/*trabajador1.cambiaNombre("Maria");*/
+		
+		System.out.println(trabajador1.devuelveDatos());
+		
+		System.out.println(trabajador2.devuelveDatos());
+		
+		System.out.println(trabajador3.devuelveDatos());
+		
+		System.out.println(trabajador4.devuelveDatos());
+
+	}
+
+}
+
+class Empleados {
+	
+	public Empleados(String nom) {
+		
+		nombre = nom;
+		seccion = "Administracion";
+		Id = IdSiguiente; //Definimos el estado inicial
+		IdSiguiente++; //Incrementamos el valor
+		
+	}
+	
+	//Metodo SETTER para la seccion
+	public void cambiaSeccion(String seccion) {
+		
+		this.seccion = seccion;
+		
+	}
+	
+	//Metodo GETTER para el nombre y la seccion
+	public String devuelveDatos() {
+		return "El nombre es: " + nombre + ", la seccion es: " + seccion + " y el id es: " + Id;
+	}
+	
+	//Metodo SETTER para el nombre
+	/*public void cambiaNombre(String nombre) {
+		this.nombre = nombre; //el error es debido a que el atributo nombre es de tipo final y no puede ser modificado
+	}*/
+	
+	private final String nombre; //final - para no poder modificar el nombre durante la ejecucion del programa (constante en JAVA)
+	private String seccion;
+	private int Id; 
+	private static int IdSiguiente = 1; //static - para dar "unicidad" a cada objeto
+	
+}
